2e4e6fd0632ec8617f648cb6c3570c9559b0f348
gdsanders <gd@digitalequity.com>
2015-11-02 22:58:37 -0600

    AddItemViewController added
    IBActions added

---
+++ CheckCheckDone/AddItemViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddItemViewController.swift
+//  CheckCheckDone
+//
+//  Created by G.D. Sanders on 11/2/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddItemViewController: UITableViewController {
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+      dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func done(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+}
+
