8b9d6dbb6ee1d97275cf77a0e2c2b9e62c20a8c8
gdsanders <gd@digitalequity.com>
2015-11-06 16:57:01 -0600

    New AllListsViewController

--- CheckCheckDone/AllListsViewController.swift
+++ CheckCheckDone/AllListsViewController.swift
@@ -6,8 +6,52 @@
 //  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class AllListsViewController: UITableViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = cellForTableView(tableView)
+        cell.textLabel!.text = "List \(indexPath.row)"
+        return cell
+
+        
+    }
+    
+    func cellForTableView(tableView: UITableView) -> UITableViewCell { let cellIdentifier = "Cell"
+        if let cell =
+        tableView.dequeueReusableCellWithIdentifier(cellIdentifier) { return cell
+        } else {
+            return UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        }
+    }
+    
+
+   
 }
