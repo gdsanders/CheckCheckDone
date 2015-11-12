6a6ca7e73fad9925edfa1f4dfe07029bf68a2c2b
gdsanders <gd@digitalequity.com>
2015-11-12 10:15:25 -0600

    ListDetailViewController added

---
+++ CheckCheckDone/ListDetailViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ListDetailViewController.swift
+//  CheckCheckDone
+//
+//  Created by G.D. Sanders on 11/11/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+import UIKit
+
+protocol ListDetailViewControllerDelegate: class {
+    
+    func listDetailViewControllerDidCancel(controller: ListDetailViewController)
+    
+    func listDetailViewController(controller: ListDetailViewController, didFinishAddingChecklist checklist: Checklist)
+    
+    func listDetailViewController(controller: ListDetailViewController, didFinishEditingChecklist checklist: Checklist)
+
+
+}
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.enabled = true
+        }
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    
+    @IBAction func done() {
+        if let checklist = checklistToEdit {
+        checklist.name = textField.text! delegate?.listDetailViewController(self,
+            didFinishEditingChecklist: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(self, didFinishAddingChecklist: checklist)
+        }
+    }
+    
+    
+    
+    
+    
+}
