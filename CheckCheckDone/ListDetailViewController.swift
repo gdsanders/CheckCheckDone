cd96862bf3b1210b1d0a053b65c4f16f41f5e592
gdsanders <gd@digitalequity.com>
2015-11-12 23:12:17 -0600

    func textField

--- CheckCheckDone/ListDetailViewController.swift
+++ CheckCheckDone/ListDetailViewController.swift
@@ -63,4 +63,12 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate {
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
         return nil
     }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange,
+        replacementString string: String) -> Bool {
+        let oldText: NSString = textField.text!
+        let newText: NSString = oldText.stringByReplacingCharactersInRange( range, withString: string)
+        doneBarButton.enabled = (newText.length > 0)
+        return true
+    }
 }
